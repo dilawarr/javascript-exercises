170231ffec9fd903a10da216523577133b1aa6b9
Dilawar Khan <dilawar2030@gmail.com>
2024-06-05 16:49:31 +0500

    Solve Task 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, timesRepeated) {
+    if(timesRepeated < 0)
+        return "ERROR";
 
+    let output = ''
+    while(timesRepeated > 0) {
+        output += string;
+        timesRepeated--;
+    }
+    return output;
 };
 
 // Do not edit below this line
