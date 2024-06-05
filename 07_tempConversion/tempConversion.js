059dbaecd8dca6f1823e348a9d42c0b1d0946f40
Dilawar Khan <dilawar2030@gmail.com>
2024-06-05 17:47:43 +0500

    Solve Task 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = ((fahrenheit - 32) * (5/9))
+  if(!Number.isInteger(result))
+    result = Number(result.toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = ((celsius * 9/5) + 32);
+  if(!Number.isInteger(result))
+    result = Number(result.toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
