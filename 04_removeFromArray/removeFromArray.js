e943b2841f7de115bc824ee60382b56c992e7d1e
Dilawar Khan <dilawar2030@gmail.com>
2024-06-05 17:14:20 +0500

    Solve Task 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+
+    for(let i = 1; i < arguments.length; i++) {
+        let index = arr.indexOf(arguments[i]);
+        while(index != -1) {
+            arr.splice(index, 1);
+            index = arr.indexOf(arguments[i]);
+        }
+    }
+
+    return arr;
+
 };
 
 // Do not edit below this line
