20f68ac19abc5de408d010d921bc0e28114dbc8e
Dilawar Khan <dilawar2030@gmail.com>
2024-06-18 02:39:25 +0500

    Solve Task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+	return array.reduce((oldest, person) => {
+		const oldestAge =
+			"yearOfDeath" in oldest
+				? oldest.yearOfDeath - oldest.yearOfBirth
+				: new Date().getFullYear() - oldest.yearOfBirth;
+		const personAge =
+			"yearOfDeath" in person
+				? person.yearOfDeath - person.yearOfBirth
+				: new Date().getFullYear() - person.yearOfBirth;
+		return oldestAge > personAge ? oldest : person;
+	});
 };
 
 // Do not edit below this line
