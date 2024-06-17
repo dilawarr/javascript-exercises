420f8965f403ff3bf4fdc8a06819c4a69b8f80ce
Dilawar Khan <dilawar2030@gmail.com>
2024-06-18 02:20:41 +0500

    Solve Task

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+	num = +num;
+	if (num == 1 || num == 2) return 1;
+	else if (num < 0) return "OOPS";
+	else if (num == 0) return 0;
 
+	let x = 1;
+	let y = 1;
+	let z = 0;
+	let i = 2;
+	while (i != num) {
+		z = x + y;
+		x = y;
+		y = z;
+		i++;
+	}
+	return z;
 };
 
 // Do not edit below this line
