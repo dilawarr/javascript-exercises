83da6b1dee6e6a6ca71fe551ed1ffa5cbb01e1a8
Dilawar Khan <dilawar2030@gmail.com>
2024-06-18 02:06:53 +0500

    Solve Task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	str = str.toLowerCase();
+	let normalizedStr = str
+		.split("")
+		.filter(
+			(elem) => (elem >= "a" && elem <= "z") || (elem >= "0" && elem <= "9")
+		)
+		.join("");
+	let start = 0;
+	let end = normalizedStr.length - 1;
+	console.log({ normalizedStr });
+	while (start < end)
+		if (normalizedStr[start] !== normalizedStr[end]) return false;
+		else start++, end--;
+	return true;
 };
 
 // Do not edit below this line
